a7ec8de79d6c13d614da5744a248a67472aea895
Nicolas Lopes <162803649+BobSkywalker@users.noreply.github.com>
2024-11-02 08:17:51 -0300

    Tudo de hoje por enquanto

---
+++ Função/arrowFunction1.js
@@ -0,0 +1,19 @@
+let triplo = function triplo(a) {
+  return 3 * a;
+};
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a // return implicito
+
+console.log(dobro(Math.PI))
+
+let ola = function () {
+    return 'ola'
+}
+
+ola = () => 'Ola'
+
+ola = _ => 'ola' // posui um param
