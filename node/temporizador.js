21a149a5eee2ed299031b0e472b1f994db1ca3c4
Nicolas Lopes <162803649+BobSkywalker@users.noreply.github.com>
2024-11-23 08:36:53 -0300

    Update temporizador.js

--- node/temporizador.js
+++ node/temporizador.js
@@ -1,5 +1,1 @@
-const schedule = require('node-schedule')
-
-const tarefa1 = schedule.scheduleJob('*/5 * 16 * * 6', function() {
-    console.log('Executando Tarefa 1!', new Date().getSeconds())
-})
+console.log("Bom dia");
