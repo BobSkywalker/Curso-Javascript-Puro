63193b40a8e9e5d5f60a83b437032ea901d56926
Nicolas Lopes <162803649+BobSkywalker@users.noreply.github.com>
2024-11-25 14:49:57 -0300

    yeup

--- Pratica/Graham Method/main.js
+++ Pratica/Graham Method/main.js
@@ -1,12 +1,14 @@
-const botao = document.getElementById('botao')
-const ativoValor = document.getElementById('valorAtivo')
-const vpa = document.getElementById('VPA')
-const lpa = document.getElementById('LPA')
-const displayRes = document.getElementById('resultado')
+const botao = document.getElementById("botao");
+const ativoValor = document.getElementById("valorAtivo");
+const vpa = document.getElementById("VPA");
+const lpa = document.getElementById("LPA");
+const displayRes = document.getElementById("resultado");
 
-botao.addEventListener('click', () => {
-     const vpaValue = vpa.value;
-     const lpaValue = lpa.value;
-     const total = Math.sqrt(22.5 * vpaValue * lpaValue).toFixed(2);
+botao.addEventListener("click", calcularGraham);
+
+function calcularGraham(){
+      const vpaValue = vpa.value;
+      const lpaValue = lpa.value;
+      const total = Math.sqrt(22.5 * vpaValue * lpaValue).toFixed(2);
       displayRes.innerHTML = `O  preço teto é: ${total}`;
-})
+}
