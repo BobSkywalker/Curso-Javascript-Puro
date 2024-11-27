d01d3e78b04f0e507fdeff6a0c002454a0d9b305
Nicolas Lopes <162803649+BobSkywalker@users.noreply.github.com>
2024-11-27 17:52:10 -0300

    Hamburguer

--- Pratica/Hamburguer Menu/main.js
+++ Pratica/Hamburguer Menu/main.js
@@ -1,6 +1,17 @@
 const botao = document.getElementById("botao");
-const conteudo = document.getElementById("conteudo");
+const conteudo = document.getElementById("container");
+let isMenuOpen = false;
 
 botao.addEventListener("click", () => {
   conteudo.classList.toggle("active");
+
+  /*if(!isMenuOpen){ // Colocando o ! no inicio da variavel ele inverte o valor dela caso for numeros, booleanos, stingy
+        conteudo.classList.toggle('active')
+       conteudo.classList.remove('deactive')
+        isMenuOpen = true
+    } else {
+        conteudo.classList.remove('active')
+        conteudo.classList.toggle('deactive')
+        isMenuOpen = false                                  
+    }*/
 });
