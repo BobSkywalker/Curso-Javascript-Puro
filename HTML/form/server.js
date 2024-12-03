5d7591e4a44d82501fbce234333213c2b28a4125
Nicolas Lopes <162803649+BobSkywalker@users.noreply.github.com>
2024-12-03 17:42:56 -0300

    Coisas Basicas

--- HTML/form/server.js
+++ HTML/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyPaser.urlencoded({ extended: true }));
 
 app.post("/usuarios", (req, res) => {
   console.log(req.body);
-  res.send("<h1>Parabens</h1>");
+  res.send("<h1>Parabens. Usuario Incluido</h1>");
+});
+
+app.post("/usuarios/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.send("<h1>Parabens. Usuario Alterado</h1>");
 });
 
 app.listen(3030);
